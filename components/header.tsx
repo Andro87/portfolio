4b34c23422a89cea7800c0563324f349f6a22fd2
Andro87 <ggandolfo987@gmail.com>
2021-10-18 11:33:59 +0200

    [Initial] -Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./header.module.scss";
+import Link from "next/link";
+import Git from "../svg/github.svg";
+import { useState } from "react";
+const Header: React.FunctionComponent = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <header
+            className={`${styles.header} ${open ? styles.menu_open : null}`}
+        >
+            <div className={styles.btn_wrap} onClick={() => setOpen(!open)}>
+                {!open ? (
+                    <button
+                        type="button"
+                        aria-label="menu button"
+                        className={styles.menu_btn}
+                    ></button>
+                ) : (
+                    <button
+                        type="button"
+                        aria-label="close button"
+                        className={styles.close_btn}
+                    ></button>
+                )}
+            </div>
+            {open ? (
+                <div className={styles.menu}>
+                    <Link href="/">
+                        <a className={styles.link}>Home</a>
+                    </Link>
+
+                    <Link href="/portfolio">
+                        <a className={styles.link}>Portfolio</a>
+                    </Link>
+
+                    <Link href="/contact">
+                        <a className={styles.link}>Contact Me</a>
+                    </Link>
+
+                    <div className={styles.link_wrap}>
+                        <Git />
+                        <img
+                            src="/images/icons/favicon-32x32.png"
+                            alt="frontend"
+                        />
+                    </div>
+                </div>
+            ) : null}
+        </header>
+    );
+};
+export default Header;
---
+++ components/work.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode } from "react";
+import styles from "./work.module.scss";
+import Right from "../svg/arrow-right.svg";
+import Left from "../svg/arrow-left.svg";
+import Link, { LinkProps } from "next/link";
+import { useState } from "react";
+
+interface Props {
+    readonly image: ReactNode;
+    readonly project: string;
+    readonly language: string;
+    readonly info: string;
+}
+const Work: React.FunctionComponent<Props> = props => {
+    const { image, project, language, info } = props;
+    const [show, setShow] = useState(false);
+    return (
+        <div className={styles.work}>
+            <div
+                className={`${styles.work_img} ${
+                    show ? styles.background : null
+                }`}
+            >
+                {image}
+            </div>
+
+            <div className={styles.work_info}>
+                <h5>{project}</h5>
+                <p>{language}</p>
+                <Link href="/">
+                    <a> SEE PROJECT</a>
+                </Link>
+            </div>
+
+            <button
+                type="button"
+                aria-label="more info"
+                className={styles.btn_more}
+                onClick={() => setShow(!show)}
+            >
+                {!show ? <Right /> : <Left />}
+            </button>
+            {show ? (
+                <div className={styles.info}>
+                    <p>{info}</p>
+                </div>
+            ) : null}
+        </div>
+    );
+};
+export default Work;
